f941b4726d8fa9f8bf8f38482a25100d106673fb
wee-owl <kem-cv@yandex.ru>
2024-06-27 09:31:26 +0300

    feat: add container component

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import style from "./Container.module.css";
+import classNames from "classnames";
+
+
+function Container({ children, className }) {
+
+  return (
+    <div className={classNames(style.container, className)}>
+      {children}
+    </div>
+  );
+}
+
+export default Container;
+
+Container.propTypes = {
+  children: PropTypes.object.isRequired,
+  className: PropTypes.string.isRequired,
+};
