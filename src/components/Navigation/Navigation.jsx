b007d5ebba285d6b27bdabc54bcd864e37e97bfd
wee-owl <kem-cv@yandex.ru>
2024-07-01 14:02:10 +0300

    feat: use dispatch to change category

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,42 +1,38 @@
 import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { changeCategory } from "../../store/category/categorySlice";
 import Container from "../Container/Container";
 import style from "./Navigation.module.css";
 import classNames from "classnames";
 
 
 function Navigation({ className }) {
+  const { category, activeCategory } = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+
 
   return (
     <nav className={style.navigation}>
       <Container className={className}>
         <ul className={style.navigation__list}>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_burger, style.navigation__button_active)}>Бургеры</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_snack)}>Закуски</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_hotdog)}>Хот-доги</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_combo)}>Комбо</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_shawarma)}>Шаурма</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_pizza)}>Пицца</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_wok)}>Вок</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_dessert)}>Десерты</button>
-            </li>
-            <li className={style.navigation__item}>
-              <button className={classNames(style.navigation__button, style.navigation__button_sauce)}>Соусы</button>
-            </li>
+          {
+            category.map((item, i) => 
+              <li className={style.navigation__item} key={i}>
+                <button 
+                  className={classNames(
+                    style.navigation__button,
+                    activeCategory === i ? style.navigation__button_active : ""
+                  )}
+                  style={{backgroundImage: `url(${item.image})`}}
+                  onClick={() => {
+                    dispatch(changeCategory({indexCategory: i}))
+                  }}
+                >
+                  {item.rus}
+                </button>
+              </li>
+            )
+          }
           </ul>
       </Container>
     </nav>
