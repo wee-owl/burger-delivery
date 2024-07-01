fe936a6fc25e34dccbb0dff0e1ff7e2c1c76cce1
wee-owl <kem-cv@yandex.ru>
2024-07-01 12:28:03 +0300

    feat: add order component

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,97 +1,38 @@
-import PropTypes from "prop-types";
-import "./order.css";
-import "./goods.css";
-import "./count.css";
+//import PropTypes from "prop-types";
+import { orderList } from "../../utils/orderList";
+import OrderGoods from "../OrderGoods/OrderGoods";
+import style from "./Order.module.css";
 
 
 function Order() {
-
   return (
-
-    <div className="catalog__order order">
-      <section className="order__wrapper">
-        <div className="order__header" tabIndex="0" role="button">
-          <h2 className="order__title">Корзина</h2>
-
-          <span className="order__count">4</span>
+    <div className={style.order}>
+      <section className={style.order__wrapper}>
+        <div className={style.order__header} tabIndex="0" role="button">
+          <h2 className={style.order__title}>Корзина</h2>
+          <span className={style.order__count}>4</span>
         </div>
 
-        <div className="order__wrap_list">
-          <ul className="order__list">
-            <li className="order__item">
-              <img className="order__image" src="img/burger_1.jpg" alt="Супер сырный" />
-
-              <div className="order__goods goods">
-                <h3 className="goods__title">Супер сырный</h3>
-
-                <p className="goods__weight">512г</p>
-
-                <p className="goods__price">1279
-                  <span className="currency">₽</span>
-                </p>
-              </div>
-
-              <div className="count">
-                <button className="count__minus">-</button>
-                <p className="count__amount">1</p>
-                <button className="count__plus">+</button>
-              </div>
-            </li>
-
-            <li className="order__item">
-              <img className="order__image" src="img/free_1.jpg" alt="Картошка фри" />
-
-              <div className="order__goods goods">
-                <h3 className="goods__title">Картошка фри</h3>
-
-                <p className="goods__weight">180г</p>
-
-                <p className="goods__price">245
-                  <span className="currency">₽</span>
-                </p>
-              </div>
-
-              <div className="count">
-                <button className="count__minus">-</button>
-                <p className="count__amount">2</p>
-                <button className="count__plus">+</button>
-              </div>
-            </li>
-
-            <li className="order__item">
-              <img className="order__image" src="img/hot-dog_1.jpg" alt="Жгучий хот-дог" />
-
-              <div className="order__goods goods">
-                <h3 className="goods__title">Жгучий хот-дог</h3>
-
-                <p className="goods__weight">245г</p>
-
-                <p className="goods__price">239
-                  <span className="currency">₽</span>
-                </p>
-              </div>
-
-              <div className="count">
-                <button className="count__minus">-</button>
-                <p className="count__amount">1</p>
-                <button className="count__plus">+</button>
-              </div>
-            </li>
+        <div className={style.order__wrap_list}>
+          <ul className={style.order__list}>
+            {
+              orderList.map((item, i) => <OrderGoods title={item} key={i}/>)
+            }
           </ul>
 
-          <div className="order__total">
+          <div className={style.order__total}>
             <p>Итого</p>
             <p>
-              <span className="order__amount">1279</span>
-              <span className="currency">₽</span>
+              <span className={style.order__amount}>1279</span>
+              <span className={style.currency}>₽</span>
             </p>
           </div>
 
-          <button className="order__submit">Оформить заказ</button>
+          <button className={style.order__submit}>Оформить заказ</button>
 
-          <div className="order__apeal">
-            <p className="order__text">Бесплатная доставка</p>
-            <button className="order__close">Свернуть</button>
+          <div className={style.order__apeal}>
+            <p className={style.order__text}>Бесплатная доставка</p>
+            <button className={style.order__close}>Свернуть</button>
           </div>
         </div>
       </section>
@@ -102,6 +43,6 @@ function Order() {
 
 export default Order;
 
-Order.propTypes = {
-  className: PropTypes.string.isRequired,
-};
+// Order.propTypes = {
+//   className: PropTypes.string.isRequired,
+// };
