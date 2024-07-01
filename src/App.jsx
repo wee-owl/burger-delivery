5d5fc3a8c350c3e347d315e736a828b5f8db4d0c
wee-owl <kem-cv@yandex.ru>
2024-07-01 13:51:48 +0300

    feat: add provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Catalog from "./components/Catalog/Catalog";
@@ -7,14 +9,14 @@ import Footer from "./components/Footer/Footer";
 function App() {
 
   return (
-    <>
+    <Provider store={store}>
       <Header className="header__container"/>
       <main>
         <Navigation className="navigation__container" />
         <Catalog />
       </main>
       <Footer />
-    </>
+    </Provider>
   );
 }
 
