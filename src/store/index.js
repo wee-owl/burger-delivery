500847be0c7b52efa095890ec14db9d57c7400a0
wee-owl <kem-cv@yandex.ru>
2024-07-03 17:12:54 +0300

    feat: add modal slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import categoryReducer from "./category/categorySlice";
 import productReducer from "./product/productSlice";
+import categoryReducer from "./category/categorySlice";
+import modalReducer from "./modalDelivery/modalDeliverySlice";
+import orderReducer, { lsmiddleware } from "./order/orderSlice";
 
 
 export const store = configureStore({
   reducer: {
     category: categoryReducer,
     product: productReducer,
+    order: orderReducer,
+    modal: modalReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(lsmiddleware), 
 });
