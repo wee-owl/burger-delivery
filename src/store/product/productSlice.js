379ae973aa585d2e402da93db8d4abe0060b640a
wee-owl <kem-cv@yandex.ru>
2024-07-17 16:45:33 +0300

    feat: update product slice

--- src/store/product/productSlice.js
+++ src/store/product/productSlice.js
@@ -4,6 +4,7 @@ import { API_URL, POSTFIX } from "../../utils/const";
 
 const initialState = {
   products: [],
+  load: false,
   error: "",
 };
 
@@ -21,13 +22,16 @@ const productSlice = createSlice({
     builder
     .addCase(productRequestAsync.pending, (state) => {
       state.error = "";
+      state.load = true;
     })
     .addCase(productRequestAsync.fulfilled, (state, action) => {
       state.error = "";
+      state.load = false;
       state.products = action.payload;
     })
     .addCase(productRequestAsync.rejected, (state, action) => {
       state.error = action.payload.error;
+      state.load = false;
     })
   }
 });
