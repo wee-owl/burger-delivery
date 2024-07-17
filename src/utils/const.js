2124ffebc5cdcbe51ccb7382bae6c36d3e79c2e2
wee-owl <kem-cv@yandex.ru>
2024-07-17 16:47:06 +0300

    feat: add api url

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3024";
+export const API_URL = "https://living-keen-makeup.glitch.me";   // https://living-keen-makeup.glitch.me || http://localhost:3024
 export const POSTFIX = "/api/product";
