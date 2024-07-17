c15651b687f5239f0d40ef13c90811f94ecab3b2
wee-owl <kem-cv@yandex.ru>
2024-07-17 16:42:49 +0300

    feat: update order component

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,24 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { orderRequestAsync } from "../../store/order/orderSlice";
 import { openModal } from "../../store/modalDelivery/modalDeliverySlice";
 import OrderGoods from "../OrderGoods/OrderGoods";
+import classNames from "classnames";
 import style from "./Order.module.css";
 
 
 function Order() {
   const { totalPrice, totalCount, orderList, orderGoods } = useSelector(state => state.order);
   const dispatch = useDispatch();
+  const [openOrder, setOpenOrder] = useState(false);
 
   useEffect(() => {
     dispatch(orderRequestAsync());
   }, [dispatch, orderList.length]);
 
+  const handleModal = () => dispatch(openModal());
+
 
   return (
-    <div className={style.order}>
+    <div className={classNames(style.order, openOrder ? style.order_open : "")}>
       <section className={style.order__wrapper}>
-        <div className={style.order__header} tabIndex="0" role="button">
+        <div 
+          className={style.order__header} 
+          tabIndex="0" 
+          role="button" 
+          onClick={() => {setOpenOrder(!openOrder)}}
+        >
           <h2 className={style.order__title}>Корзина</h2>
           <span className={style.order__count}>{totalCount}</span>
         </div>
@@ -41,14 +50,20 @@ function Order() {
           <button 
             className={style.order__submit} 
             disabled={!orderGoods.length}
-            onClick={() => dispatch(openModal())}
+            onClick={handleModal}
           >
             Оформить заказ
           </button>
 
           <div className={style.order__apeal}>
             <p className={style.order__text}>Бесплатная доставка</p>
-            <button className={style.order__close}>Свернуть</button>
+            <button 
+              className={style.order__close} 
+              type="button" 
+              onClick={() => {setOpenOrder(!openOrder)}}
+            >
+                Свернуть
+            </button>
           </div>
         </div>
       </section>
